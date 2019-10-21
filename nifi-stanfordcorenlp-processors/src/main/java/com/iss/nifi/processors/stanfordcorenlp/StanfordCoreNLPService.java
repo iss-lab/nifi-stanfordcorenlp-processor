0f1e96f0b433817eb139e6ca7f5b1f37511b6695
Drew Kerrigan <andrew.kerrigan@issgovernance.com>
2019-10-21 17:08:00 -0400

    Updated nlp service to optionally include embedded server for faster processing

--- nifi-stanfordcorenlp-processors/src/main/java/com/iss/nifi/processors/stanfordcorenlp/StanfordCoreNLPService.java
+++ nifi-stanfordcorenlp-processors/src/main/java/com/iss/nifi/processors/stanfordcorenlp/StanfordCoreNLPService.java
@@ -1,29 +1,66 @@
 package com.iss.nifi.processors.stanfordcorenlp;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
+import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.CoreDocument;
 import edu.stanford.nlp.pipeline.CoreEntityMention;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.pipeline.StanfordCoreNLPClient;
+import edu.stanford.nlp.pipeline.StanfordCoreNLPServer;
 
 public class StanfordCoreNLPService {
   public static final String[] locationKeys = "STATE_OR_PROVINCE,CITY".split(",");
   public static final String[] organizationKeys = "ORGANIZATION".split(",");
 
-	public Map<String, List<String>> analyze(String text, String annotators, String format) {
+  private StanfordCoreNLPServer server;
+
+  public StanfordCoreNLPService() throws IOException {
+    this(null, true);
+  }
+
+  public StanfordCoreNLPService(Properties props, boolean embeddedServer) throws IOException {
+    if (! embeddedServer) {
+      return;
+    }
+    if (props == null) {
+      props = new Properties();
+    }
+    server = new StanfordCoreNLPServer(props);
+    server.run();
+  }
+
+  public StanfordCoreNLPClient getClient(Properties props) {
+    if (props == null) {
+      props = new Properties();
+    }
+    InetSocketAddress addr = server.getServer().get().getAddress();
+    StanfordCoreNLPClient client = new StanfordCoreNLPClient(props, "http://" + addr.getHostString(), addr.getPort());
+    return client;
+  }
+
+	public Map<String, List<String>> analyze(String text, String annotators)  {
     List<String> locations = new ArrayList<String>();
     List<String> organizations = new ArrayList<String>();
 
     Properties props = new Properties();
     props.setProperty("annotators", annotators);
-    StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-    CoreDocument document = new CoreDocument(text);
-    pipeline.annotate(document);
-    
+
+    // StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+    // CoreDocument document = new CoreDocument(text);
+    // pipeline.annotate(document);
+
+    StanfordCoreNLPClient pipeline = getClient(props);
+    Annotation annotation = pipeline.process(text);
+    System.out.println("Creating core doc");
+    CoreDocument document = new CoreDocument(annotation);
+
     for (CoreEntityMention entityMention: document.entityMentions()) {
       for (String k : locationKeys) {
         if (k.equals(entityMention.entityType())) {
@@ -46,7 +83,13 @@ public class StanfordCoreNLPService {
   
   public static void main(String[] args) {
     String text = "Example Text.";
-    StanfordCoreNLPService svc = new StanfordCoreNLPService();
-    System.out.println(svc.analyze(text, "tokenize,ssplit,pos,lemma,ner,parse", ""));
+    System.out.println("Analyzing text: " + text);
+    try {
+      StanfordCoreNLPService svc = new StanfordCoreNLPService();
+      System.out.println(svc.analyze(text, "tokenize,ssplit,pos,lemma,ner"));
+      System.exit(0);
+    } catch (Exception e) {
+      System.out.println("Encountered exception while analyzing text: " + e);
+    }
   }
 }
